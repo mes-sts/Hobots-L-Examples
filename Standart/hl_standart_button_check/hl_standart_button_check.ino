1e50cd0de8f42209f78ab677965bfffcee1c9f2f
Roman <romansklyar15@gmail.com>
2024-06-11 16:24:20 +0300

    Create hl_standart_button_check.ino

---
+++ Standart/hl_standart_button_check/hl_standart_button_check.ino
@@ -0,0 +1,33 @@
+/*
+ * hl_standart_button_check
+ * 
+ * Проверка концевого прерывателя (кнопки) HL Стандарт
+ * 
+ * Кнопка подключена в разъем 2
+ * 
+ * Инструменты → Плата → Arduino Uno
+ * 
+ * Copyright (C) 2024. v1.0 / MES
+ */
+
+
+// Вывод управления сервоприводом
+const int BUTTON_PIN = 3;
+
+void setup() {
+  // Настройка скорости отладочного порта
+  Serial.begin(9600);
+
+  // Настройка вывода кнопки на вход
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN)) {
+    Serial.println("Button PRESSED");
+  }
+  else {
+    Serial.println("Button RELEASED");
+  }
+  delay(20);
+}
