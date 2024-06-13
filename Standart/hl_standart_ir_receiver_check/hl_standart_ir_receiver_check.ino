3cb54475998de5175fbb15b8c0fdce05aa191f5e
Roman <romansklyar15@gmail.com>
2024-06-13 16:06:18 +0300

    Create hl_standart_ir_receiver_check.ino

---
+++ Standart/hl_standart_ir_receiver_check/hl_standart_ir_receiver_check.ino
@@ -0,0 +1,43 @@
+/*
+ * hl_standart_ir_receiver_check
+ * 
+ * Проверка инфракрасного приемника HL Стандарт
+ * 
+ * Ссылка на библиотеку https://github.com/Arduino-IRremote/Arduino-IRremote/tree/master
+ * 
+ * Инструменты → Плата → Arduino Uno
+ * 
+ * Copyright (C) 2024. v1.0 / MES
+ */
+
+// Подключение библиотеки для работы с ИК пультами
+#include <IRremote.hpp>
+
+// Вывод ИК приемника
+const int IR_SENSOR_PIN = 8;
+ 
+void setup() {
+  // Настройка скорости отладочного порта
+  Serial.begin(9600);
+  // Инициализация приемника
+  IrReceiver.begin(IR_SENSOR_PIN);
+}
+
+void loop() {
+  if (IrReceiver.decode()) {
+    if (IrReceiver.decodedIRData.protocol == UNKNOWN) { // Приняты данные неизвестного протокола (возможно не указаного) или шум
+      Serial.println("Received noise or an unknown (or not yet enabled) protocol");
+      IrReceiver.resume();
+    }
+    else {
+      IrReceiver.resume();
+      Serial.print("Address ");
+      Serial.println(IrReceiver.decodedIRData.address, HEX); // Значение адреса
+      Serial.print("Command ");
+      Serial.println(IrReceiver.decodedIRData.command, HEX); // Значение команды
+      Serial.print("Raw ");
+      Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX); // Значение всей принятой информации
+    }
+    Serial.println();
+  }
+}
